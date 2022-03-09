1c8d4d7b6ac15fd461daaf34e6cdf62da8bbbc3d
GanescuRobert <studentGanescuRobertFlorinel@gmail.com>
2022-03-09 12:03:41 +0200

    MainActivity layout, Countdown timer, Intents for activities

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 
 ## References
 
+## Dependencies
+
+[google/flexbox-layout](https://github.com/google/flexbox-layout)
 [PhilJay/MPAndroidChart](https://github.com/PhilJay/MPAndroidChart)
+
+## Videos
 
 | Nr. | Tutorials                                                                                    |
 | --- | -------------------------------------------------------------------------------------------- |
 | 1.  | [Stacked Bar Chart](https://www.youtube.com/watch?v=jTYi0Q7lLco&ab_channel=SarthiTechnology) |
-| 2.  | [Pie Chart](https://www.youtube.com/watch?v=S3zqxVoIUig&t=631s&ab_channel=LearntoDroid)      |
+| 2.  | [Pie Chart](https://www.youtube.com/watch?v=S3zqxVoIUig&ab_channel=LearntoDroid)             |
+| 3.  | [Countdown Timer](https://www.youtube.com/watch?v=MDuGwI6P-X8&ab_channel=CodinginFlow)       |
--- app/src/main/java/com/example/pomodoro/MainActivity.java
+++ app/src/main/java/com/example/pomodoro/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.pomodoro;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    private static final long START_TIME_IN_MILLIS = 600000;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimerRunning;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+        mButtonStartPause = findViewById(R.id.start_pause_button);
+        mButtonReset = findViewById(R.id.reset_button);
+
+        updateCountDownText();
     }
 
 
@@ -18,4 +37,65 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, StatisticsActivity.class);
         startActivity(intent);
     }
+
+    public void openCreatePomodoroActivity(View view) {
+        Intent intent = new Intent(this, CreatePomodoroActivity.class);
+        startActivity(intent);
+    }
+
+    public void startPauseButton(View view) {
+        if (mTimerRunning) {
+            pauseTimer();
+        } else {
+            startTimer();
+        }
+    }
+
+    public void resetButton(View view) {
+        resetTimer();
+
+    }
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long l) {
+                mTimeLeftInMillis = l;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+        mTimerRunning = true;
+        mButtonStartPause.setText("Pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) mTimeLeftInMillis / 1000 / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+
 }
