3e15c083c9945867fb8f8e40c18ad331f3a7ca7b
VladDob <vladdobritoiu0@gmail.com>
2022-03-21 18:27:16 +0200

    SwipeAdapter layouts and class

---
+++ app/src/main/java/com/example/pomodoro/SwipeAdapter.java
@@ -0,0 +1,69 @@
+package com.example.pomodoro;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+import java.util.ArrayList;
+
+public class SwipeAdapter extends PagerAdapter {
+    private Context context;
+    private ArrayList<Pomodoro> modelArrayList;
+
+    public SwipeAdapter(Context context, ArrayList<Pomodoro> modelArrayList) {
+        this.context = context;
+        this.modelArrayList = modelArrayList;
+    }
+
+    @Override
+    public int getCount() {
+        return modelArrayList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_view_pager, container, false);
+
+        TextView cardTitle= view.findViewById(R.id.cardTitle);
+        //View banner = view.findViewById(R.id.banner);
+
+        //get data
+        Pomodoro model = modelArrayList.get(position);
+        String name = model.getName();
+        Integer focus = model.getFocus();
+        Integer short_break = model.getShort_break();
+        Integer long_break = model.getLong_break();
+        Integer sets = model.getSets();
+        Integer sets_until_long_break = model.getSets_until_long_break();
+
+        //set data
+        cardTitle.setText(name);
+
+        // handle click
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // To do
+            }
+        });
+        // add to container
+        container.addView(view, position);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
